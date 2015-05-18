37fd0f73834d242abf9f06128a51d8ad49b9f07f
Alooy yousif <alooy16@gmail.com>
2015-05-18 14:34:34 +0300

    Create util_client.lua

---
+++ util_client.lua
@@ -0,0 +1,36 @@
+function dxDrawEmptyRec(startX, startY, endX, endY, color, width, postGUI)
+	dxDrawLine ( startX, startY, startX+endX, startY, color, width, postGUI )
+	dxDrawLine ( startX, startY, startX, startY+endY, color, width, postGUI )
+	dxDrawLine ( startX, startY+endY, startX+endX, startY+endY,  color, width, postGUI )
+	dxDrawLine ( startX+endX, startY, startX+endX, startY+endY, color, width, postGUI )
+end
+
+function dxDrawText_(text,x,y,w,h,color,scale,font,alignX,alignY,clip,wordBreak,postGUI,colorCoded)
+	dxDrawText(text,x,y,x + w,y + h,color,scale,font,alignX,alignY,clip or false,wordBreak or false,postGUI or true,colorCoded or true)
+end
+
+function isMouseInPosition ( x, y, width, height )
+    if ( not isCursorShowing ( ) ) then
+        return false
+    end
+ 
+    local sx, sy = guiGetScreenSize ( )
+    local cx, cy = getCursorPosition ( )
+    local cx, cy = ( cx * sx ), ( cy * sy )
+    if ( cx >= x and cx <= x + width ) and ( cy >= y and cy <= y + height ) then
+        return true
+    else
+        return false
+    end
+end
+
+function RGBToHex(red, green, blue, alpha)
+	if((red < 0 or red > 255 or green < 0 or green > 255 or blue < 0 or blue > 255) or (alpha and (alpha < 0 or alpha > 255))) then
+		return nil
+	end
+	if(alpha) then
+		return string.format("#%.2X%.2X%.2X%.2X", red,green,blue,alpha)
+	else
+		return string.format("#%.2X%.2X%.2X", red,green,blue)
+	end
+end
